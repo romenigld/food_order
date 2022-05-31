f169cc68f5b1ccc4d5c9c1ec45ea73f9674f4fe1
Romenig Lima Damasio <romenigld@gmail.com>
2022-05-31 19:48:28 +0200

    prepared the coverage

--- lib/food_order_web/router.ex
+++ lib/food_order_web/router.ex
@@ -10,9 +10,9 @@ defmodule FoodOrderWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
+  # pipeline :api do
+  #   plug :accepts, ["json"]
+  # end
 
   scope "/", FoodOrderWeb do
     pipe_through :browser
@@ -25,6 +25,7 @@ defmodule FoodOrderWeb.Router do
   #   pipe_through :api
   # end
 
+  # coveralls-ignore-start
   # Enables LiveDashboard only for development
   #
   # If you want to use the LiveDashboard in production, you should put
@@ -41,6 +42,8 @@ defmodule FoodOrderWeb.Router do
       live_dashboard "/dashboard", metrics: FoodOrderWeb.Telemetry
     end
   end
+
+  # coveralls-ignore-stop
 
   # Enables the Swoosh mailbox preview in development.
   #
