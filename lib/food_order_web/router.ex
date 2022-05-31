12bbb62d8ce478b3aa9bd3ab0c3a89aa623753a5
Romenig Lima Damasio <romenigld@gmail.com>
2022-05-31 22:36:54 +0200

    fixed the sobelow

--- lib/food_order_web/router.ex
+++ lib/food_order_web/router.ex
@@ -7,7 +7,7 @@ defmodule FoodOrderWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, {FoodOrderWeb.LayoutView, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
   end
 
   # pipeline :api do
