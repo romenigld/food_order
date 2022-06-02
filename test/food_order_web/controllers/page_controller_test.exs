bd4137bc16c61b6b7843aa6e34342cb1fd0b2b2b
Romenig Lima Damasio <romenigld@gmail.com>
2022-06-02 20:23:10 +0200

    fixed the tests

--- test/food_order_web/controllers/page_controller_test.exs
+++ test/food_order_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule FoodOrderWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Tailwind CSS"
   end
 end
