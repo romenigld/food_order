7e8a7d1d177f9db71384feac2950b6877b514300
Romenig Lima Damasio <romenigld@gmail.com>
2022-06-06 21:35:40 +0200

    2.2 - Frontend Liveview com Tailwind - Criando Components

--- lib/food_order_web/live/main/main_live.ex
+++ lib/food_order_web/live/main/main_live.ex
@@ -1,5 +1,7 @@
 defmodule FoodOrderWeb.MainLive do
   use FoodOrderWeb, :live_view
+  alias FoodOrderWeb.Main.Hero
+  alias FoodOrderWeb.Main.Items
 
   def mount(_params, _session, socket) do
     {:ok, socket}
