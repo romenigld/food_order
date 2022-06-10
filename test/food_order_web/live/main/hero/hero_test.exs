79e876b7888ce2b19ec1c7de00761cd2b01e2e03
Romenig Lima Damasio <romenigld@gmail.com>
2022-06-10 18:21:52 +0200

    2.2 - Frontend Liveview com Tailwind - Testes em Components Utilizando Liveview

---
+++ test/food_order_web/live/main/hero/hero_test.exs
@@ -0,0 +1,14 @@
+defmodule FoodOrderWeb.Main.HeroTest do
+  use FoodOrderWeb.ConnCase, async: true
+  import Phoenix.LiveViewTest
+
+  test "load hero", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.main_path(conn, :index))
+
+    assert has_element?(view, "#hero-component")
+    assert has_element?(view, "[data-role=hero-info]", "Make your order")
+    assert has_element?(view, "[data-id=recomendation]", "Make your order")
+    assert has_element?(view, "[data-id=cta]", "Right now!")
+    assert has_element?(view, "[data-id=cta-button]", "Order now")
+  end
+end
---
+++ test/food_order_web/live/main/items/item/item_test.exs
@@ -0,0 +1,14 @@
+defmodule FoodOrderWeb.Main.Items.ItemTest do
+  use FoodOrderWeb.ConnCase, async: true
+  import Phoenix.LiveViewTest
+
+  test "load item", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.main_path(conn, :index))
+
+    assert has_element?(view, "#item-0")
+    assert has_element?(view, "[data-role=product-img][data-id=item-0]")
+    assert has_element?(view, "[data-role=product-description]")
+    assert has_element?(view, "[data-role=product-price][data-id=item-1]", "$ 10")
+    assert has_element?(view, "[data-role=product-add][data-id=item-1]")
+  end
+end
