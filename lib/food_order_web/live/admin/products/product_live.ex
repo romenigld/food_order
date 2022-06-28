f5517b676ec2398b67215a95fb26f75686c9b469
Romenig Lima Damasio <romenigld@gmail.com>
2022-06-28 11:54:12 +0200

    exercise JS hide modal completed

--- lib/food_order_web/live/admin/products/product_live.ex
+++ lib/food_order_web/live/admin/products/product_live.ex
@@ -5,7 +5,11 @@ defmodule FoodOrderWeb.Admin.ProductLive do
 
   def mount(_params, _session, socket) do
     products = Products.list_products()
-
     {:ok, socket |> assign(products: products)}
+  end
+
+  def handle_params(params, _url, socket) do
+    IO.inspect params
+    {:noreply, socket}
   end
 end
