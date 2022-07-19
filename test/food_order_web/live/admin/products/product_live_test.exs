7af76ffa4796de15bb9090239db4425085fd0a62
Romenig Lima Damasio <romenigld@gmail.com>
2022-07-20 00:14:06 +0200

    2.4 -  Autenticação - Layout Paginas Continuação

--- test/food_order_web/live/admin/products/product_live_test.exs
+++ test/food_order_web/live/admin/products/product_live_test.exs
@@ -3,45 +3,49 @@ defmodule FoodOrderWeb.ProductLiveTest do
   import Phoenix.LiveViewTest
   import FoodOrder.Factory
 
-  test "load page", %{conn: conn} do
-    product = insert(:product)
+  describe "test default page product" do
+    setup :register_and_log_in_user
 
-    # {:ok, view, _html} = live(conn, "/admin/products")
-    {:ok, view, _html} = live(conn, Routes.admin_product_path(conn, :index))
-    assert has_element?(view, "[data-role=product-section]")
-    assert has_element?(view, "[data-role=product-table]")
-    assert has_element?(view, "[data-id=head-name]", "Name")
-    assert has_element?(view, "[data-id=head-price]", "Price")
-    assert has_element?(view, "[data-id=head-size]", "Size")
-    assert has_element?(view, "[data-id=head-actions]", "Actions")
-    assert has_element?(view, "[data-role=product-list]")
+    test "load page", %{conn: conn} do
+      product = insert(:product)
 
-    # test product
-    assert has_element?(view, "[data-role=product-item][data-id=#{product.id}]")
-    assert has_element?(view, "[data-role=product-name][data-id=#{product.id}]", product.name)
+      # {:ok, view, _html} = live(conn, "/admin/products")
+      {:ok, view, _html} = live(conn, Routes.admin_product_path(conn, :index))
+      assert has_element?(view, "[data-role=product-section]")
+      assert has_element?(view, "[data-role=product-table]")
+      assert has_element?(view, "[data-id=head-name]", "Name")
+      assert has_element?(view, "[data-id=head-price]", "Price")
+      assert has_element?(view, "[data-id=head-size]", "Size")
+      assert has_element?(view, "[data-id=head-actions]", "Actions")
+      assert has_element?(view, "[data-role=product-list]")
 
-    assert has_element?(
-             view,
-             "[data-role=product-price][data-id=#{product.id}]",
-             "R$2.00"
-           )
+      # test product
+      assert has_element?(view, "[data-role=product-item][data-id=#{product.id}]")
+      assert has_element?(view, "[data-role=product-name][data-id=#{product.id}]", product.name)
 
-    assert has_element?(view, "[data-role=product-size][data-id=#{product.id}]", product.size)
+      assert has_element?(
+               view,
+               "[data-role=product-price][data-id=#{product.id}]",
+               "R$2.00"
+             )
 
-    assert has_element?(view, "[data-role=product-actions][data-id=#{product.id}]")
-  end
+      assert has_element?(view, "[data-role=product-size][data-id=#{product.id}]", product.size)
 
-  test "given a product that has already exists when click to delete then removes the product from the db",
-       %{conn: conn} do
-    product = insert(:product)
+      assert has_element?(view, "[data-role=product-actions][data-id=#{product.id}]")
+    end
 
-    {:ok, view, _html} = live(conn, Routes.admin_product_path(conn, :index))
-    assert has_element?(view, "[data-role=delete][data-id=#{product.id}]", "Delete")
+    test "given a product that has already exists when click to delete then removes the product from the db",
+         %{conn: conn} do
+      product = insert(:product)
 
-    assert view
-           |> element("[data-role=delete][data-id=#{product.id}]", "Delete")
-           |> render_click()
+      {:ok, view, _html} = live(conn, Routes.admin_product_path(conn, :index))
+      assert has_element?(view, "[data-role=delete][data-id=#{product.id}]", "Delete")
 
-    refute has_element?(view, "[data-role=delete][data-id=#{product.id}]")
+      assert view
+             |> element("[data-role=delete][data-id=#{product.id}]", "Delete")
+             |> render_click()
+
+      refute has_element?(view, "[data-role=delete][data-id=#{product.id}]")
+    end
   end
 end
