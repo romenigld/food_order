500cfef8674b8161cd619355c47fb44b1e20e4e4
Romenig Lima Damasio <romenigld@gmail.com>
2022-07-20 12:25:54 +0200

    2.4 -  Autenticação - Ecto Enum

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,18 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     FoodOrder.Repo.insert!(%FoodOrder.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias FoodOrder.Accounts
+
+Accounts.register_user(%{
+  email: "adm@elxpro.com",
+  password: "Adm@elxpro.com",
+  role: "ADMIN"
+})
+
+Accounts.register_user(%{
+  email: "user@elxpro.com",
+  password: "User@elxpro.com",
+  role: "USER"
+})
+
+Accounts.register_user(%{
+  email: "romenig@elxpro.com",
+  password: "Romenig@elxpro.com"
+})
